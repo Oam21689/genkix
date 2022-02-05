96772e02a2b8277264d8ec35fb1be6da330e3cad
Olame Muliri <olamemuliri@Olames-MacBook-Pro.local>
2022-02-05 12:11:58 -0600

    update

---
+++ genkix.py
@@ -0,0 +1,10 @@
+# This python program generates ssh-keys
+# Extract public key for use on servers such as github
+# Copy your public key and past it where ever its required
+# The diverse, the safer
+
+import subprocess
+import os
+
+generate_key = subprocess.run(["ssh-keygen", "-o"])
+hereIsThekey = os.system("cat ~/.ssh/id_rsa.pub")
